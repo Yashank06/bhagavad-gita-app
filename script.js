01bdc6b05f87b07196b4b05b23cfe0a8e9be2d29
YASHANK MASKI <maskiyashank88@gmail.com>
2025-03-07 19:32:38 +0530

    updated script

--- script.js
+++ script.js
@@ -131,7 +131,7 @@ window.onload = displayRandomVerse;
 // Register the service worker
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('/service-worker.js')
       .then((registration) => {
         console.log('Service Worker registered:', registration);
       })
